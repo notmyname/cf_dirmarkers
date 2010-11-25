abba853f0fb5029a85ee04448d70667afdc80624
John Dickinson <jdickinson@racklabs.com>
2010-11-25 11:12:03 -0600

    removed cloud files language bindings

--- make_dirmarkers.py
+++ make_dirmarkers.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python
 
 import httplib
-import cloudfiles
 
 import cf_auth
 
 CDN_LOGS = '.CDN_ACCESS_LOGS'
 
-conn = cloudfiles.get_connection(username=cf_auth.username,
-                                 api_key=cf_auth.apikey)
 
+# auth
+conn = httplib.HTTPSConnection('api.mosso.com')
+conn.request('GET', '/auth', headers={'x-auth-user': cf_auth.username,
+                                      'x-auth-key': cf_auth.apikey})
+resp = conn.getresponse()
+auth_token = resp.getheader('x-auth-token')
+url = resp.getheader('x-storage-url')
+conn.close()
+container_path = '/'+'/'.join(url.split('/')[3:])+'/' + CDN_LOGS
+conn = httplib.HTTPSConnection(url.split('/')[2])
+
+def get_listing(marker=None):
+    global conn
+    query = container_path
+    if marker:
+        query += '?marker=%s' % marker
+    conn.request('GET', query, headers={'X-Auth-Token': auth_token})
+    resp = conn.getresponse()
+    return [x for x in resp.read().split('\n') if x]
 
 prefixes = set()
-container = conn.get_container(CDN_LOGS)
-last = None
 results = ['foo']
+last = None
 while len(results):
-    results = container.list_objects(marker=last)
+    results = get_listing(marker=last)
     if not results:
         break
     last = results[-1]
@@ -29,12 +44,10 @@ while len(results):
             if parts not in results:
                 prefixes.add(parts)
 
-url, _, auth_token = conn.auth.authenticate()
-httpconn = httplib.HTTPSConnection(url.split('/')[2])
 send_headers = {'X-Auth-Token': auth_token, 'Content-Type': 'application/directory'}
 container_path = '/' + '/'.join(url.split('/')[3:]) + '/' + CDN_LOGS
 print len(prefixes), 'objects to create'
 for i, p in enumerate(sorted(prefixes)):
-    httpconn.request('PUT', container_path+'/'+p, headers=send_headers)
-    httpconn.getresponse().read()
+    conn.request('PUT', container_path+'/'+p, headers=send_headers)
+    conn.getresponse().read()
     print i
